b292457fbcd6986f08b7aacc80fd862b4b5bccb3
cach30verfl0w <contact@cach30verfl0w.net>
2026-07-06 19:04:59 +0200

    feat: Enable publishing, all targets and apply conventions plugin

--- kotlin-library-core/build.gradle.kts
+++ kotlin-library-core/build.gradle.kts
@@ -1,7 +1,37 @@
+import net.cacheoverflow.conventions.apache2License
+import net.cacheoverflow.conventions.defaultDevelopers
+import net.cacheoverflow.conventions.defaultPublishing
+import net.cacheoverflow.conventions.extension.configureJava
+import net.cacheoverflow.conventions.extension.defaultCompilerOptions
+import net.cacheoverflow.conventions.extension.withJvm
+import net.cacheoverflow.conventions.extension.withNative
+import net.cacheoverflow.conventions.projectMetadata
+
 plugins {
     alias(sharedLibs.plugins.kotlin.multiplatform)
+    alias(sharedLibs.plugins.mavenPublish)
+    alias(sharedLibs.plugins.conventions)
+    `maven-publish`
+}
+
+configureJava(sharedLibs.versions.jvmTarget.get().toInt())
+defaultPublishing {
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            defaultDevelopers()
+            apache2License()
+            projectMetadata(
+                name = project.name,
+                description = property("project.description") as String,
+                repository = property("project.repository") as String
+            )
+        }
+    }
 }
 
 kotlin {
-    jvm()
+    defaultCompilerOptions()
+    withNative()
+    withJvm()
+    applyDefaultHierarchyTemplate()
 }
